125343e8ce894a51d249ae05e0d55265255eb494
zzzzarrakexeaa <143167198+zzzzarrakexeaa@users.noreply.github.com>
2023-09-02 09:48:35 +0300

    Update Builder.bat

--- Blank Grabber/Builder.bat
+++ Blank Grabber/Builder.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0
 
-title Checking Python installation...
+pyhton indiriliyor ...
 python --version > nul 2>&1
 if %errorlevel% neq 0 (
     echo Python is not installed! (Go to https://www.python.org/downloads and install the latest version.^)
